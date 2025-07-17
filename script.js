f911dc2f0fb5aa3634715e8c4afa95aa57e02d9a
HarshAgarwal100 <ha9329187@gmail.com>
2025-07-17 16:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function sendLove() {
+    const response = document.getElementById("response");
+    response.innerText = "good to know This came from my heart 🤍";
+    response.style.display = "block";
+  }
+  
